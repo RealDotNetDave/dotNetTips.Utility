9061b7fd2885f6f6f288519ad83e3d34eabb32e0
David McCarter <dotnetdave@live.com>
2017-05-23 16:23:24 -0700

    Modified starting and stopping services to async.

--- dotNetTips.Utility/AssemblyInfo.vb
+++ dotNetTips.Utility/AssemblyInfo.vb
@@ -10,7 +10,8 @@ Imports System.Security.Permissions
 <Assembly: AssemblyCopyright("David McCarter - dotNetTips.com © 2017")>
 <Assembly: AssemblyTrademark("David McCarter - dotNetTips.com")>
 <Assembly: CLSCompliant(True)>
-<Assembly: AssemblyVersion("2017.3.6333.39208")>
+<Assembly: AssemblyVersion("2017.5.6352.41619")>
 <Assembly: ComVisible(False)>
 <Assembly: NeutralResourcesLanguageAttribute("en-US")>
-<Assembly: AssemblyFileVersion("2017.3.6333.39208")>
+<Assembly: AssemblyFileVersion("2017.5.6352.41619")>
+
