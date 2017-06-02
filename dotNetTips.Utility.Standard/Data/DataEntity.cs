7c97523c56c14eebd16aef20343d0d70d1d8c1f6
David McCarter <dotnetdave@live.com>
2017-06-02 07:19:18 -0700

    Added new EF entity base class.

--- dotNetTips.Utility.Standard/Data/DataEntity.cs
+++ dotNetTips.Utility.Standard/Data/DataEntity.cs
@@ -1,6 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// ***********************************************************************
+// Assembly         : 
+// Author           : David McCarter
+// Created          : 06-01-2017
+//
+// Last Modified By : David McCarter
+// Last Modified On : 06-02-2017
+// ***********************************************************************
+// <copyright file="DataEntity.cs" company="dotNetTips.com - David McCarter">
+//     dotNetTips.com - David McCarter
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotNetTips.Utility.Standard.Data
 {
@@ -10,7 +23,7 @@ namespace dotNetTips.Utility.Standard.Data
     public abstract class DataEntity
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="DataEntity"/> class.
+        /// Initializes a new instance of the <see cref="DataEntity" /> class.
         /// </summary>
         protected DataEntity()
         {
@@ -26,7 +39,7 @@ namespace dotNetTips.Utility.Standard.Data
         public DateTimeOffset CreatedAt { get; set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="DataEntity"/> is deleted.
+        /// Gets or sets a value indicating whether this <see cref="DataEntity" /> is deleted.
         /// </summary>
         /// <value><c>true</c> if deleted; otherwise, <c>false</c>.</value>
         [Column(Order = 96)]
