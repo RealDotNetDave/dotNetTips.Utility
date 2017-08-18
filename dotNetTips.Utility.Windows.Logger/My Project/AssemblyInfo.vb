a661310d3e4e7998629192d2973b1686c0719f8a
David McCarter <davidmccarter@live.com>
2017-08-18 20:24:43 +0000

    NuGet packages update.

--- dotNetTips.Utility.Windows.Logger/My Project/AssemblyInfo.vb
+++ dotNetTips.Utility.Windows.Logger/My Project/AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyProduct("dotNetTips.Utility.Windows.Logger")>
 <Assembly: AssemblyCopyright("David McCarter - dotNetTips.com © 2017")>
 <Assembly: AssemblyTrademark("David McCarter - dotNetTips.com")>
-<Assembly: AssemblyVersion("2017.8.16.0043")>
+<Assembly: AssemblyVersion("2017.8.18.2023")>
 <Assembly: ComVisible(False)>
 <Assembly: NeutralResourcesLanguageAttribute("en-US")>
-<Assembly: AssemblyFileVersion("2017.8.16.0043")>
+<Assembly: AssemblyFileVersion("2017.8.18.2023")>
